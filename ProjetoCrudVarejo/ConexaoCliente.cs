680487bd42a019d528e78198e565b4e8e2a8a759
Alvaro Pires de Souza <ap4321316@gmail.com>
2026-09-09 22:18:32 -0300

    fix: parametriza comandos de cliente e gerencia conexao com segurança

--- ProjetoCrudVarejo/ConexaoCliente.cs
+++ ProjetoCrudVarejo/ConexaoCliente.cs
@@ -1,73 +1,58 @@
-﻿using MySql.Data.MySqlClient;
+using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjetoCrudVarejo
 {
     internal class ConexaoCliente
     {
-        MySqlConnection conn;
+        private const string ConnectionString = "Persist Security info=false;server=localhost;database=projetocrudvarejo;user=root;pwd=;";
 
-        public void ConectarBD()
+        public bool ExecutarComandos(string sql, params MySqlParameter[] parametros)
         {
             try
             {
-                conn = new MySqlConnection("Persist Security info = false; server = localhost; database = projetocrudvarejo; user=root; pwd=;");
-                conn.Open();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+                using (var conn = new MySqlConnection(ConnectionString))
+                using (var cmd = new MySqlCommand(sql, conn))
+                {
+                    if (parametros != null && parametros.Length > 0)
+                    {
+                        cmd.Parameters.AddRange(parametros);
+                    }
 
-        }
-        public bool ExecutarComandos(string sql)
-        {
-            try
-            {
-                //Abrir conexão
-                ConectarBD();
-                //Preparar o comadno
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                //Executar comando
-                cmd.ExecuteNonQuery();
-
-
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
             }
             catch (Exception ex)
             {
-                throw new Exception("Erro ao executar o comando: " + ex.Message);
+                throw new Exception("Erro ao executar o comando no banco de dados.", ex);
             }
-            finally
-            {
-                conn.Close();
-            }
-            return true;
         }
-        public DataTable ExecutarConsulta(string sql)
+
+        public DataTable ExecutarConsulta(string sql, params MySqlParameter[] parametros)
         {
             try
             {
-                ConectarBD();
-                MySqlDataAdapter da = new MySqlDataAdapter(sql, conn);
-                DataTable dt = new DataTable();
-                da.Fill(dt);
-                return dt;
+                using (var conn = new MySqlConnection(ConnectionString))
+                using (var cmd = new MySqlCommand(sql, conn))
+                using (var da = new MySqlDataAdapter(cmd))
+                {
+                    if (parametros != null && parametros.Length > 0)
+                    {
+                        cmd.Parameters.AddRange(parametros);
+                    }
 
+                    var dt = new DataTable();
+                    da.Fill(dt);
+                    return dt;
+                }
             }
             catch (Exception ex)
             {
-                throw new Exception("Erro ao executar a consulta: " + ex.Message);
-            }
-            finally
-            {
-                conn.Close();
+                throw new Exception("Erro ao executar a consulta no banco de dados.", ex);
             }
         }
     }
-
 }
